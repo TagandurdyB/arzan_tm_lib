11d2ffb94eb2e107f6053cc08b1cb203cefe6ebb
Tagandurdy <114348033+TagandurdyB@users.noreply.github.com>
2023-04-12 21:14:44 +0300

    Update my_icons.dart

--- config/vars/my_icons.dart
+++ config/vars/my_icons.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class IconsLight {
   static const Icon main = Icon(Icons.home);
   Icon menu = const Icon(Icons.menu);
-  Icon location = const Icon(Icons.location_on_outlined);
+  Icon location = const Icon(Icons.placemark);
   Icon add = const Icon(Icons.add_box_outlined);
   Icon home = const Icon(Icons.home_filled);
   Icon profile = const Icon(Icons.assignment_ind_outlined);
